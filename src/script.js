eff722d31e28a1a938bcaca04b32970eba830589
TyDine22 <150012901+TyDine22@users.noreply.github.com>
2023-12-11 07:35:22 +0000

    implemented a few scientific functions

--- src/script.js
+++ src/script.js
@@ -10,7 +10,15 @@ document.addEventListener("DOMContentLoaded", function () {
         const convertedValue = currentValue
                                 .replace("×", "*")
                                 .replace("÷", "/")
-                                .replace("%", "*0.01");
+                                .replace("%", "*0.01")
+                                .replace("sin", "Math.sin")
+                                .replace("cos", "Math.cos")
+                                .replace("ln", "Math.log")
+                                .replace("π", "Math.pi")
+                                .replace("log", "Math.log10")
+                                .replace("e", "Math.E")
+                                .replace("tan", "Math.tan")
+                                .replace("√", "Math.sqrt");
         console.log('converted value', convertedValue);
         const result = eval(convertedValue);
         currentValue = result.toString();
@@ -22,17 +30,25 @@ document.addEventListener("DOMContentLoaded", function () {
         button.addEventListener('click', function () {
             const value = button.innerText;
 
-            if (value == "AC") {
-                currentValue = "";
-                display.value = currentValue;
-            } 
-            else if (value == "=") {
-                evaluateResult();
-            } 
-            else {
-                currentValue += value;
+            try {
+                if (value == "AC") {
+                    currentValue = "";
+                    display.value = currentValue;
+                } 
+                else if (value == "=") {
+                    evaluateResult();
+                } 
+                else {
+                    currentValue += value;
+                    display.value = currentValue;
+                }
+            } catch (error) {
+                console.error(error);
+                currentValue = "ERROR";
                 display.value = currentValue;
             }
+
+            
         })
         
     }
