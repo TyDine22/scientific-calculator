85976568647404d572351c41b5cb98006c307c25
TyDine22 <150012901+TyDine22@users.noreply.github.com>
2023-12-11 07:01:53 +0000

    seperated out the script file

---
+++ src/script.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Document is ready");
+    const display = document.getElementById('calc-display');
+    const buttons = document.getElementsByClassName('btn');
+    
+    let currentValue = "";
+
+    function evaluateResult () {
+        console.log('current value', currentValue);
+        const convertedValue = currentValue
+                                .replace("×", "*")
+                                .replace("÷", "/")
+                                .replace("%", "*0.01");
+        console.log('converted value', convertedValue);
+        const result = eval(convertedValue);
+        currentValue = result.toString();
+        display.value = currentValue;
+    }
+
+    for (let i = 0; i < buttons.length; i++) {
+        const button = buttons[i];
+        button.addEventListener('click', function () {
+            const value = button.innerText;
+
+            if (value == "AC") {
+                currentValue = "";
+                display.value = currentValue;
+            } 
+            else if (value == "=") {
+                evaluateResult();
+            } 
+            else {
+                currentValue += value;
+                display.value = currentValue;
+            }
+        })
+        
+    }
+    
+});
